7a0c307934603b91d165aadd47beb8e47cd6acda
Numan Dirican <diricannuman@gmail.com>
2023-03-27 09:47:27 +0300

    Mongodb and nextauth connected

---
+++ pages/api/[...nextauth].ts
@@ -0,0 +1,53 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import prismaDB from "@/lib/prismadb";
+import { compare } from "bcrypt";
+import { debug } from "console";
+export default NextAuth({
+  providers: [
+    Credentials({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Email or Password is required");
+        }
+        const user = await prismaDB.user.findunique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user.email || !user.hashedPassword) {
+          throw new Error("Email does not exist.");
+        }
+
+        const isCorrectPassword = await compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error("Incorrect password");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth",
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SESSION_SECRET,
+});
