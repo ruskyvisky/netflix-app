7027d2814db5234031b03791c8d752d2f61e96a2
ruskyvisky <diricannuman@gmail.com>
2023-03-28 19:02:24 +0300

    Fea : Fetcher and serverAuth Added

---
+++ lib/serverAuth.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest } from "next";
+import { getSession } from "next-auth/react";
+
+import prismadb from "./prismadb";
+
+const serverAuth = async (req:NextApiRequest) =>{
+    const session = await  getSession({req})
+
+    if(!session?.user?.email){
+        throw new Error("Not authenticated")
+    }
+
+    const currentUser = await prismadb.user.findFirst({
+        where:{
+            email: session.user.email
+        }
+    })
+
+    if(!currentUser){
+        throw new Error("not authorized")
+    }
+
+    return {currentUser}
+}
+
+export default serverAuth
